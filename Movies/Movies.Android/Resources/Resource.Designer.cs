7348f98e5e4a91fb42526eb6fddb766ca2c586cb
Luis Alberto Peña Nuñez <luis.pena.nunez.developer@gmail.com>
2017-09-30 19:23:51 -0500

    Set the pink background for the Genre

--- Movies/Movies.Android/Resources/Resource.Designer.cs
+++ Movies/Movies.Android/Resources/Resource.Designer.cs
@@ -5645,8 +5645,11 @@ namespace Movies.Droid
 			// aapt resource value: 0x7f0200a3
 			public const int mr_ic_play_light = 2130837667;
 			
+			// aapt resource value: 0x7f0200a5
+			public const int notification_template_icon_bg = 2130837669;
+			
 			// aapt resource value: 0x7f0200a4
-			public const int notification_template_icon_bg = 2130837668;
+			public const int thatpinkthough = 2130837668;
 			
 			static Drawable()
 			{
